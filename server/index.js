866901cf81fccb4b9216fdd9bd995f76bcf6b108
marwan <marwan.04e@gmail.com>
2024-11-30 13:12:25 +0200

    Initial commit of sic-assembler project

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
